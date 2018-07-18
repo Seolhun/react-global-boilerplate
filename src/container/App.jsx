d5d754b231d0cd9fe57dabacd469b5073560ec09
Seolhun <shun10114@gmail.com>
2018-07-19 01:15:27 +0900

    [Container] Simplify container

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,15 +1,24 @@
 import React, { Fragment } from 'react';
 
-import HomeView from './home';
-
+import { Header, Sidebar } from '../component/layout';
 import Routes, { schema } from '../routes';
+
 
 const App = () => (
   <Fragment>
-    <HomeView />
-    <main>
-      <Routes depth={1} routes={schema} />
-    </main>
+    <div className="row">
+      <aside className="col-sm-2">
+        <Sidebar />
+      </aside>
+      <header className="col-sm-8">
+        <Header depth={1} routes={schema} />
+      </header>
+    </div>
+    <div>
+      <main>
+        <Routes depth={1} routes={schema} />
+      </main>
+    </div>
   </Fragment>
 );
 
